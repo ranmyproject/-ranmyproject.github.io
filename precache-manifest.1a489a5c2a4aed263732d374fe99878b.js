4a96ece36fc871a4f8d484de8d9fd240187ad838
Raj Kumar Meena <raj.meena@mettl.com>
2020-06-14 18:58:15 +0530

    Updates

---
+++ precache-manifest.1a489a5c2a4aed263732d374fe99878b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a7b0bb314d1151416d4980c9d0cfc4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46bc2db3d7407cc476dc",
+    "url": "/static/css/2.627926fe.chunk.css"
+  },
+  {
+    "revision": "b9647b65ff695f07da5f",
+    "url": "/static/css/main.ae8ef47a.chunk.css"
+  },
+  {
+    "revision": "46bc2db3d7407cc476dc",
+    "url": "/static/js/2.ecae2ec6.chunk.js"
+  },
+  {
+    "revision": "97557dac625cf0680a71c54f9bece515",
+    "url": "/static/js/2.ecae2ec6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9647b65ff695f07da5f",
+    "url": "/static/js/main.c2cc6129.chunk.js"
+  },
+  {
+    "revision": "d30f027682679ce6213a",
+    "url": "/static/js/runtime-main.7bfe45fd.js"
+  }
+]);
